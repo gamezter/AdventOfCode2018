6a3d6e31078a4e5693ed01a2c8cb1cb38aee81b8
Alexandre Charron <alexandre.charron@illogika.com>
2018-12-21 15:16:38 -0500

    add padding to map to avoid branches in hot loop

--- AdventOfCode2018/Day18.cs
+++ AdventOfCode2018/Day18.cs
@@ -90,34 +90,34 @@ namespace AdventOfCode2018
         {
             string[] lines = new StreamReader("day18.txt").ReadToEnd().Trim().Split('\n');
 
-            char[][] state = new char[50][];
+            char[][] state = new char[52][];
+            state[0] = new char[52];
+            state[51] = new char[52];
             for (int y = 0; y < 50; y++)
             {
-                state[y] = new char[50];
+                state[y + 1] = new char[52];
                 for (int x = 0; x < 50; x++)
                 {
-                    state[y][x] = lines[y][x];
+                    state[y + 1][x + 1] = lines[y][x];
                 }
             }
 
             for (int i = 0; i < 1000000000; i++)
             {
-                char[][] nextState = new char[50][];
-                for (int y = 0; y < 50; y++)
+                char[][] nextState = new char[52][];
+                nextState[0] = new char[52];
+                nextState[51] = new char[52];
+                for (int y = 1; y < 51; y++)
                 {
-                    nextState[y] = new char[50];
-                    for (int x = 0; x < 50; x++)
+                    nextState[y] = new char[52];
+                    for (int x = 1; x < 51; x++)
                     {
                         int trees = 0, yard = 0;
 
                         for (int dx = -1; dx < 2; dx++)
                         {
-                            if (x + dx < 0 || x + dx > 49)
-                                continue;
                             for (int dy = -1; dy < 2; dy++)
                             {
-                                if (y + dy < 0 || y + dy > 49)
-                                    continue;
                                 if (dx == 0 && dy == 0)
                                     continue;
                                 char c = state[y + dy][x + dx];
@@ -145,7 +145,7 @@ namespace AdventOfCode2018
                 
                 Console.CursorLeft = 0;
                 Console.CursorTop = 0;
-                for (int y = 0; y < 50; y++)
+                for (int y = 0; y < 52; y++)
                 {
                     Console.WriteLine(new string(nextState[y]));
                 }
@@ -154,9 +154,9 @@ namespace AdventOfCode2018
             }
 
             int wood = 0, lumberyard = 0;
-            for (int y = 0; y < 50; y++)
+            for (int y = 1; y < 51; y++)
             {
-                for (int x = 0; x < 50; x++)
+                for (int x = 1; x < 51; x++)
                 {
                     char c = state[y][x];
                     if (c == '|')
